5d9cad6637ae5675d8ae494dbc9d3f43ede2ad74
Hung Anh <anhlhpl52119@gmail.com>
2024-07-09 21:20:08 +0700

    feat: style

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,19 +2,18 @@
   <div>
     <canvas
       id="gradient-canvas"
-      style="clip-path: polygon(100px 0px, 100% 0px, calc(100% + 225px) 100%, 250px 100%); filter: blur(0px);"
       data-transition-in="true"
-      class="isLoaded fixed right-[-2px] top-0 z-10 h-screen w-[80%] shadow-abd md:w-1/2"
-      width="454"
-      height="600"
+      class="isLoaded fixed right-[-2px] top-10% z-10 h-80vh md:w-1/3"
+      width="300"
+      height="800"
     />
 
-    <div class="relative z-20 uppercase">
+    <div class="relative z-20 ml-10% uppercase">
       <div class="mt-20% h-300 p-32 px-12">
         <TransitionGroup
           name="fade"
           tag="ul"
-          class="mt-16 max-w-60% text-6.2vw font-900 lg:text-70"
+          class="mt-16 max-w-60% text-6.1vw font-900 lg:text-70"
           mode="out-in"
         >
           <li
@@ -24,7 +23,7 @@
           >
             <p>Đừng FOMO.</p>
             <p>
-              Đầu tư hiệu quả bằng kiến thức.
+              Đầu tư hiệu quả <span class="block">bằng kiến thức.</span>
             </p>
           </li>
 
@@ -32,16 +31,18 @@
             v-show="display"
             key="2"
           >
-            <p>Chọn dự án chất lượng</p>
-            <p class="inline-block">
-              đừng chọn số lượng.
+            <p class="block whitespace-nowrap">
+              Chọn dự án <span>chất lượng</span>
+            </p>
+            <p class="block whitespace-nowrap">
+              đừng chọn <span>số lượng.</span>
             </p>
           </li>
         </transitiongroup>
         <!--        <div class="absolute left-10% top-50% mt-32 flex flex-wrap gap-16"> -->
         <div class="mt-32 flex flex-wrap gap-16">
           <AButton
-            class="h-60 w-300 text-24 text-highlight font-600"
+            class="h-60 w-300 b-highlight text-24 text-highlight font-600"
             @click="$router.push({ name: 'Details' })"
           >
             Thông tin khoá học
@@ -65,12 +66,11 @@ const display = ref<boolean>(false);
 
 setInterval(() => {
   display.value = !display.value;
-}, 2000);
+}, 3500);
 </script>
 
 <style scoped lang="less">
 .isLoaded {
-  margin: 2% 0;
   background-image: url("../assets/images/111-Photoroom.png");
   background-size: cover;
   background-repeat: no-repeat;
