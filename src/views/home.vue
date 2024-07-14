33b2a1172305490a44ad1b527e6964427efc001a
Hung Anh <anhlhpl52119@gmail.com>
2024-07-14 23:35:48 +0700

    chore: transition group

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,89 @@
 <template>
-  <div class="main-page">
-    <div class="folder text-red">
-      <div class="p-32">
+  <div class="main-page main-warp">
+    <!-- folder1 span 4 -->
+    <div
+      class="col-span-4 row-span-2"
+      md="row-span-4"
+    >
+      <div class="folder bg-gray">
+        <div class="h-full flex p-16">
+          <!-- Slogan -->
+          <div
+            class="flex-1 text-center"
+            md="text-left"
+          >
+            <TransitionGroup
+              name="fade"
+              tag="ul"
+              class="mt-16 max-w-60% text-6vw font-900 lg:text-70"
+              mode="out-in"
+            >
+              <li
+                v-show="!display"
+                key="1"
+                class="text-highlight"
+              >
+                <p>Đừng FOMO.</p>
+                <p>
+                  Đầu tư hiệu quả bằng kiến thức.
+                </p>
+              </li>
+
+              <li
+                v-show="display"
+                key="2"
+              >
+                <p>Chọn dự án chất lượng</p>
+                <p class="inline-block">
+                  đừng chọn số lượng.
+                </p>
+              </li>
+            </transitiongroup>
+          </div>
+
+          <!-- Image -->
+          <div
+            class="hidden"
+            md="block flex-basis-1/5"
+          >
+            Image
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- card video span 2 -->
+    <div
+      class="col-span-4 row-span-2 rounded-25 bg-white text-red shadow-xl"
+      md="col-span-2"
+    >
+      <div class="h-full p-16">
+        <div class="mx-auto h-inherit overflow-hidden rounded-10">
+          <iframe
+            class="h-inherit w-full"
+            src="https://www.youtube.com/embed/mMUYSM9daF4"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- card course name span 1 -->
+    <div
+      class="col-span-2 row-span-2 rounded-25 bg-white text-red shadow-xl"
+      md="col-span-1"
+    >
+      <div class="p-16">
+        <h3>hello this is content</h3>
+        <div>hello this is ok</div>
+      </div>
+    </div>
+
+    <!-- card other name span 1 -->
+    <div
+      class="col-span-2 row-span-2 rounded-25 bg-white text-red shadow-xl"
+      md="col-span-1"
+    >
+      <div class="p-16">
         <h3>hello this is content</h3>
         <div>hello this is ok</div>
       </div>
@@ -10,59 +92,31 @@
 </template>
 
 <script lang="ts" setup>
+const display = ref<boolean>(false);
+
+setInterval(() => {
+  display.value = !display.value;
+}, 2000);
 </script>
 
 <style scoped lang="less">
+.main-warp {
+  @apply max-w-1200 mx-auto p-16 mt-60;
+  @apply grid gap-16 grid-cols-4 grid-rows-6;
+  min-height: calc(100vh - 12rem);
+}
+
 .folder{
-  width: 80%;
-  height: 310px;
-  margin: 10em auto 10em;
+  @apply rounded-25 relative h-full;
+  filter: drop-shadow(0 0 0.75rem grey); // TODO: research it
+  &::before {
+    @apply absolute w-300 h-60 rounded-tl-25;
+    content:  '';
+    top: -39px;
+    right: 0;
+    background: rgb(145, 252, 149);
+    // clip-path: path('M 0 0 L 160 0 C 193 0, 165 53, 245 43 L 0 60 z');
 
-  border-radius: 5px 25px 25px 25px;
-  filter: drop-shadow(0 0 0.75rem grey);
-  background: rgb(145, 252, 149);
-  position: relative;
+  }
 }
-
-.folder::before{
-  content:  '';
-  position: absolute;
-  top: -18px;
-  width: 200px;
-  height: 25px;
-  background: rgb(145, 252, 149);
-  border-radius: 25px 0 0 0;
-  clip-path: path('M 0 0 L 160 0 C 185 2, 175 16, 200 18 L 0 50 z');
-}
-
-// .banner-warp {
-//   background-image: url('../assets//images/offer-background.jpg');
-//   background-repeat: no-repeat;
-//   background-size: cover;
-// }
-
-// .proposal-highlight {
-//   position: relative;
-//   &::after {
-//     content: "";
-//     z-index: -1;
-//     bottom: -7px;
-//     opacity: .8;
-//     left: -2px;
-//     @apply absolute rounded-3/2 w-full h-1/2 shadow-lg shadow-gray;
-//   }
-// }
-
-// .btn-course {
-//   @apply h-50 text-20 font-600 min-w-200;
-// }
-
-// :deep(.slick-slide) {
-//   @apply h-350 overflow-hidden text-white p-26 ;
-//   @apply bg-cover;
-//   background-image: url("../assets/images/bg-card3.jpg");
-//   div:has(#carousel-warp) {
-//     @apply h-full;
-//   }
-// }
 </style>
